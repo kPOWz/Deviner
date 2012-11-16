55d70e96f880968c7c3d8b0c5a4165322a9ad4ea
Katherine Zeman <karrie.zeman@gmail.com>
2012-11-15 19:04:16 -0600

    set up root level index file for trace

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+// remove the following line when in production mode
+// enables all Yii::trace logging
+//defined('YII_DEBUG') or define('YII_DEBUG',true);
+
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';

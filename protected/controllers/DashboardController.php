9be2fb717f03bdc55384e9864706b409335ff39b
Katherine Zeman <karrie.zeman@gmail.com>
2012-12-01 10:45:19 -0600

    added dashboard controller

---
+++ protected/controllers/DashboardController.php
@@ -0,0 +1,27 @@
+<?php
+class DashboardController extends Controller
+{
+
+
+	/**
+	 * This is the default 'index' action that is invoked
+	 * when an action is not explicitly requested by users.
+	 */
+	public function actionIndex()
+	{
+
+		$this->render('index');
+	}
+	
+}
+
+
+
+/**
+ * $view = 'invoice_quicken';
+			$name = $model->ID . '_invoice.iif';
+			$mime = 'text/iif';
+			$model->attachBehavior('quickbooks', 'application.behaviors.Job.QBInitializer');						
+			Yii::app()->request->sendFile($name, $this->renderPartial($view, $params, true), $mime);	
+	
+ */

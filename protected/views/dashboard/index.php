c008b14d27d7f5ef28e961f536504c1019bba4ef
Katherine Zeman <karrie.zeman@gmail.com>
2012-12-01 10:46:18 -0600

    added base view for dashboard

---
+++ protected/views/dashboard/index.php
@@ -0,0 +1,9 @@
+<?php
+$this->pageTitle=Yii::app()->name . ' - Dashboard';
+$this->breadcrumbs=array(
+		'Dashboard',
+);
+?>
+<h1>Dashboard</h1>
+
+<?php echo $this->renderPartial('//job/_export')?>

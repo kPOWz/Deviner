c57ff896dfc4adccf092004929e79c3a0533b65c
Katherine Zeman <karrie.zeman@gmail.com>
2012-12-07 11:38:10 -0600

    added inventory item header to quickbooks export files

--- protected/behaviors/QuickBooks/QBHeader.php
+++ protected/behaviors/QuickBooks/QBHeader.php
@@ -41,12 +41,44 @@ class QBHeader extends QBTransactionLine{
 		return $record;
 	}
 	
+	protected function createInventoryHeader(){
+	
+		$record['HDR'] = '!INVITEM';
+		$record['NAME']= 'NAME';
+		$record['REFNUM']= 'REFNUM';
+		$record['TIMESTAMP']= 'TIMESTAMP';
+		$record['INVITEMTYPE']= 'INVITEMTYPE';
+		$record['DESC'] = 'DESC';
+		$record['PURCHASEDESC']= 'PURCHASEDESC';
+		$record['ACCNT']= 'ACCNT';
+		$record['ASSETACCNT']= 'ASSETACCNT';
+		$record['COGSACCNT']= 'COGSACCNT';
+		$record['PRICE']= 'PRICE';
+		$record['COST']= 'COST';
+		$record['TAXABLE']= 'TAXABLE';
+		$record['PAYMETH']= 'PAYMETH';
+		$record['TAXVEND']= 'TAXVEND';
+		$record['TAXDIST']= 'TAXDIST';
+		$record['PREFVEND']= 'PREFVEND';
+		$record['REORDERPOINT']= 'REORDERPOINT';
+		$record['EXTRA']= 'EXTRA';
+		$record['CUSTFLD1']= 'CUSTFLD1';
+		$record['CUSTFLD2']= 'CUSTFLD2';
+		$record['CUSTFLD3']= 'CUSTFLD3';
+		$record['CUSTFLD4']= 'CUSTFLD4';
+		$record['CUSTFLD5']= 'CUSTFLD5';
+		$record['DEP_TYPE']= 'DEP_TYPE';
+		$record['ISPASSEDTHRU']= 'ISPASSEDTHRU';
+		
+		return $record;
+	}
 	
 	protected function createRecords(){
 
 		$lines = array();
 		$lines[] = $this->createDocumentation();
 		$lines[] = $this->createHeader();
+		$lines[] = $this->createInventoryHeader();
 		return $lines;
 	}
 

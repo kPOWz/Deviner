bc9c2968c0a5ceec2f697498b8e5e6fe8bf679a6
kPOWz <karrie.zeman@gmail.com>
2015-02-09 14:41:26 -0600

    only administrators may access reports dashboard

--- protected/controllers/DashboardController.php
+++ protected/controllers/DashboardController.php
@@ -1,7 +1,34 @@
 <?php
 class DashboardController extends Controller
 {
+	/**
+	 * @return array action filters
+	 */
+	public function filters()
+	{
+		return array(
+			'accessControl', // perform access control for CRUD operations
+		);
+	}
 
+	/**
+	 * Specifies the access control rules.
+	 * This method is used by the 'accessControl' filter.
+	 * @return array access control rules
+	 */
+	public function accessRules()
+	{
+		return array(
+			array('allow',
+				'actions'=>array('index'),
+				'users'=>array('@'),
+				'expression'=>"Yii::app()->user->getState('isAdmin');",
+			),
+			array('deny',  // deny all users
+				'users'=>array('*'),
+			),
+		);
+	}
 
 	/**
 	 * This is the default 'index' action that is invoked

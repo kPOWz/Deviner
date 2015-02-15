239b596223df5b48f18fc1ef11d7b5e5c076cb6c
kPOWz <karrie.zeman@gmail.com>
2015-02-15 13:13:17 -0600

    implemented show/hide behavior for admin navigation using bootstrap collapse components

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -5,10 +5,10 @@
 <?php Yii::app()->clientScript->registerScriptFile($this->scriptDirectory . 'flashMessages.js', CClientScript::POS_END);?>
 
 <div id="wrapper" class="row">
-	<section class="col-md-2 inverse gus-nav" id='outterContainer' >				
+	<section class="col-md-2 inverse gus-nav-container" id='outterContainer' >				
 		<?php $isAdmin = Yii::app()->user->getState('isAdmin');?>
-		<nav class="navbar navbar-vertical row" >
-			<div class="navbar-header">				
+		<nav class="navbar navbar-vertical row gus-nav" >
+			<div class="navbar-header">		
 				<h1><a href="#"><?php echo CHtml::encode(Yii::app()->name); ?></a></h1>			
 			</div>
 			<?php if(!Yii::app()->user->isGuest){?>
@@ -25,44 +25,49 @@
 					'lastItemCssClass'=>'lastmenu',
 				)); ?>
 			</nav>
-			<nav id="nav-admin" class="collapse navbar-collapse">
-				<?php 
-					if($isAdmin){
-						$this->widget('zii.widgets.CMenu', array(
-							'htmlOptions'=>array( 'id'=>'admin-nav'),
-							'items'=>array(
-								array('label'=>'Colors, Etc.', 'url'=>array('/lookup/index', 'Color'=>1, 'Style'=>1, 'Size'=>1)),
-								array('label'=>'View Vendors', 'url'=>array('/vendor/index')),
-								array('label'=>'View Customers', 'url'=>array('/customer/index')),
-								array('label'=>'View Users', 'url'=>array('/user/index')),
-								array('encodeLabel'=>false, 'label'=>'View Products<strong class="caret"></strong>', 'url'=>array('/product/index')
-									, 'items'=>$this->products
-									, 'itemOptions'=>array('class'=>'dropdown')
-									, 'linkOptions'=>array('class'=>'dropdown-toggle', 'data-toggle'=>'dropdown')),
-								array('label'=>'Add Vendor', 'url'=>array('/vendor/create')),
-								array('label'=>'Add User', 'url'=>array('/user/create')),
-								array('label'=>'Add Product', 'url'=>array('/product/create')),
-							),
-							'htmlOptions'=>array('class'=>'nav navbar-nav'),
-							'lastItemCssClass'=>'lastmenu',
-						));
-					}
-				?>
-			</nav>
-			<?php }?>
+			<div id="nav-bottom">
+				<nav id="nav-admin" class="collapse">
+					<?php 
+						if($isAdmin){
+							$this->widget('zii.widgets.CMenu', array(
+								'htmlOptions'=>array( 'id'=>'admin-nav'),
+								'items'=>array(
+									array('label'=>'Colors, Etc.', 'url'=>array('/lookup/index', 'Color'=>1, 'Style'=>1, 'Size'=>1)),
+									array('label'=>'View Vendors', 'url'=>array('/vendor/index')),
+									array('label'=>'View Customers', 'url'=>array('/customer/index')),
+									array('label'=>'View Users', 'url'=>array('/user/index')),
+									array('encodeLabel'=>false, 'label'=>'View Products<strong class="caret"></strong>', 'url'=>array('/product/index')
+										, 'items'=>$this->products
+										, 'itemOptions'=>array('class'=>'dropdown')
+										, 'linkOptions'=>array('class'=>'dropdown-toggle', 'data-toggle'=>'dropdown')),
+									array('label'=>'Add Vendor', 'url'=>array('/vendor/create')),
+									array('label'=>'Add User', 'url'=>array('/user/create')),
+									array('label'=>'Add Product', 'url'=>array('/product/create')),
+								),
+								'htmlOptions'=>array('class'=>'nav navbar-nav'),
+								'lastItemCssClass'=>'lastmenu',
+							));
+						}
+					?>
+				</nav>
+				<?php }?>
+				
+				<div class="row">
+					Hey, <?php echo Yii::app()->user->name;?>!
+					<?php if($isAdmin){?>
+						<button id="cog" type="button" class="btn btn-inverse btn-inline" title='access admin tasks'
+							data-toggle="collapse" data-target="#nav-admin">
+							<span class="glyphicon glyphicon-cog text-primary" aria-hidden="true"></span>
+						</button>
+					<?php }?>
+				</div>
+				<div class="row gray">
+					<?php $this->widget('application.widgets.SalesReportWidget'); ?>
+				</div>			
+			</div>
 		</nav>
-		<div class="row">
-			Hey, <?php echo Yii::app()->user->name;?>!
-			<?php if($isAdmin){?>
-				<button id="cog" type="button" class="btn btn-inverse btn-inline" title='access admin tasks'>
-					<span class="glyphicon glyphicon-cog text-primary" aria-hidden="true"></span>
-				</button>
-			<?php }?>
-		</div>
-		<div class="row gray">
-			<?php $this->widget('application.widgets.SalesReportWidget'); ?>
-		</div>
 	</section>
+	
 	<main class="col-md-10" id="main">		
 		<?php if(Yii::app()->user->hasFlash('success')){?>
 			<div class="flash-success" id="flash-success" >

b1011dbbc2c2e6a8c7c09f29e188bf5ce708dce9
kPOWz <karrie.zeman@gmail.com>
2015-02-17 11:46:23 -0600

    addition of GUS primary font - Avenir, close #97

--- protected/views/layouts/_header.php
+++ protected/views/layouts/_header.php
@@ -8,10 +8,10 @@
 	<!--[if lt IE 8]>
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
 	<![endif]-->
-
+	<script type="text/javascript" src="http://fast.fonts.net/jsapi/447f25d3-cc3d-43e0-a550-3dab00d3cb83.js"></script>
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-
+	
 	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>

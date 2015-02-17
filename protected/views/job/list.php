1978f2a7144089e5782b5fc9106fc12eab39d354
kPOWz <karrie.zeman@gmail.com>
2015-02-16 21:39:24 -0600

    progress on #95, implment progress-bar-esque jobs by status tabs

--- protected/views/job/list.php
+++ protected/views/job/list.php
@@ -10,10 +10,12 @@ Yii::app()->clientScript->registerCoreScript('jquery.ui');
 <?php 
 $this->widget('yiistrap.widgets.TbTabs', array(
 	'id'=>'job-tabs',
+    'htmlOptions'=>array('menuOptions'=>array('class'=>'row')),
     'tabs'=>array(
         array(
             'id'=>'job-tab-created',
             'active'=>true,
+            'htmlOptions'=>array('class'=>'col-md-2'),
             'label'=>'1. Created',
             'content'=>$this->renderPartial('_list', array(
                 'statusId'=>Job::CREATED,
@@ -21,6 +23,7 @@ $this->widget('yiistrap.widgets.TbTabs', array(
         ),
         array(
             'id'=>'job-tab-ordered',
+            'htmlOptions'=>array('class'=>'col-md-2'),
             'active'=>false,
             'label'=>'2. Ordered',
             'content'=>$this->renderPartial('_list', array(
@@ -30,6 +33,7 @@ $this->widget('yiistrap.widgets.TbTabs', array(
 
         array(
             'id'=>'job-tab-counted',
+            'htmlOptions'=>array('class'=>'col-md-2'),
             'active'=>false,
             'label'=>'3. Counted',
             'content'=>$this->renderPartial('_list', array(
@@ -39,6 +43,7 @@ $this->widget('yiistrap.widgets.TbTabs', array(
 
         array(
             'id'=>'job-tab-printed',
+            'htmlOptions'=>array('class'=>'col-md-2'),
             'active'=>false,
             'label'=>'4. Printed',
             'content'=>$this->renderPartial('_list', array(
@@ -48,6 +53,7 @@ $this->widget('yiistrap.widgets.TbTabs', array(
 
         array(
             'id'=>'job-tab-invoiced',
+            'htmlOptions'=>array('class'=>'col-md-2'),
             'active'=>false,
             'label'=>'5. Invoiced',
             'content'=>$this->renderPartial('_list', array(
@@ -57,6 +63,7 @@ $this->widget('yiistrap.widgets.TbTabs', array(
 
         array(
             'id'=>'job-tab-completed',
+            'htmlOptions'=>array('class'=>'col-md-2'),
             'active'=>false,
             'label'=>'6. Completed',
             'content'=>$this->renderPartial('_list', array(

53ca81d3e948ebb6cc3c6e15c427a9c0eefa716b
kPOWz <karrie.zeman@gmail.com>
2015-02-19 18:58:56 -0600

    disable on-page logging

--- protected/config/main.php
+++ protected/config/main.php
@@ -90,10 +90,10 @@ return array(
 				),
 				// uncomment the following to show log messages on web pages
 				
-				array(
-					'class'=>'CWebLogRoute',
-					'levels'=>'error, warning, info, trace, vardump',
-				),
+				// array(
+				// 	'class'=>'CWebLogRoute',
+				// 	'levels'=>'error, warning, info, trace, vardump',
+				// ),
 				
 			),
 		),

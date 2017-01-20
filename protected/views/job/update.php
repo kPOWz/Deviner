90bb183ddeb5facfdccded453396a0bc4c4a0f3f
kPOWz <karrie.zeman@gmail.com>
2017-01-19 19:21:45 -0600

    techd: breadcrumbs nor cleanup used any longer

--- protected/views/job/update.php
+++ protected/views/job/update.php
@@ -1,18 +1,3 @@
-<?php
-$this->breadcrumbs=array(
-	'Jobs'=>array('index'),
-	$model->ID=>array('view','id'=>$model->ID),
-	'Update',
-);
-
-$this->menu=array(
-	array('label'=>'List Job', 'url'=>array('index')),
-	array('label'=>'Create Job', 'url'=>array('create')),
-	array('label'=>'View Job', 'url'=>array('view', 'id'=>$model->ID)),
-	array('label'=>'Manage Job', 'url'=>array('admin')),
-);
-?>
-
 <h1>Update Job <small><?php echo $model->NAME; ?></small></h1>
 
 <?php echo $this->renderPartial('_form', array(
